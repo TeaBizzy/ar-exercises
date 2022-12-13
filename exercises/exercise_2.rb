ae2a617a416fdd34ca59627f65f7c0608d14aa0a
Stefan Talbot <satalbot@protonmail.com>
2022-12-13 12:26:53 -0500

    Complete Exercise
    Change the name of the first store in the db.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.first
+store2 = Store.find_by(id: 2)
+
+store1.update(name: "Victoria")
+
+puts "The name of the store at id: 1 is: #{Store.first.name}"
