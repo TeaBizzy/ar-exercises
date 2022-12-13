af680242ff81de350e83f67d065d8e48e16db104
Stefan Talbot <satalbot@protonmail.com>
2022-12-13 13:17:03 -0500

    Complete Exercise
    Selects and prints all mens stores.
    Selects and prints all womens stores with revenue > 1mil

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+mens_stores = Store.where(mens_apparel: true)
+
+mens_stores.each do |store| 
+  puts "Mens store in #{store.name}, revenue is: #{store.annual_revenue}"
+end
+
+high_revenue_womens_stores = Store.where(womens_apparel: true).where.not(annual_revenue: ..1000000)
+
+high_revenue_womens_stores.each do |store| 
+  puts "Womens store in #{store.name}, revenue is: #{store.annual_revenue}"
+end
